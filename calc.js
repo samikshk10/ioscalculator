9f1613f664080751aa26f2ae3ea69ffe49a8c864
samik shk <samik.shakya8848@gmail.com>
2022-09-24 15:21:36 +0545

    keypress

--- calc.js
+++ calc.js
@@ -1,12 +1,45 @@
 let result=document.getElementById('inputtext');
 var count =0;
+document.onkeydown=e=>{
+var keybtn=e.key;
+const keys =['0','1','2','3','4','5','6','7','8','9','*','/','%','-','+','.','(',')'];
+
+if(keys.indexOf(keybtn)!=-1)
+{
+display(keybtn);
+}
+
+
+else if(keybtn=='Enter')
+{
+    res();
+}
+
+else if(keybtn=='Backspace')
+{
+    del();
+}
+
+else if(keybtn=='c' || keybtn=='C')
+{
+    clr();
+}
+
+    
+}
+
+
+
 let display=n=>{
     count++;
     if(count==1)
     {
     result.value="";
     result.value+=n;
+
 }
+
+
 
 else{
     
@@ -65,8 +98,6 @@ function del()
         }
     }
 }
-
-
 
 
 
